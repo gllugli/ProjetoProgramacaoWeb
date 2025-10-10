d646428ed17c32df41e439b14e1d5f1b841dbf43
gllugli <gabriel.lugli@gmail.com>
2025-10-10 13:54:12 -0300

    Teste extensão Flake8

--- sgea_project/admin.py
+++ sgea_project/admin.py
@@ -1,12 +1,16 @@
-# Arquivo feito com auxílio do Chat. Aprendi essa maneira de teste durante o processo de programar o BD
+# Arquivo feito com auxílio do Chat.
+
+# Aprendi essa maneira de teste durante o processo de programar o BD
 
 from django.contrib import admin
 from .models import Usuario, Evento, Inscricao, Certificado
+
 
 @admin.register(Usuario)
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('nome', 'telefone', 'instituicao', 'perfil')
     search_fields = ('nome', 'email')
+
 
 @admin.register(Evento)
 class EventoAdmin(admin.ModelAdmin):
@@ -14,12 +18,19 @@ class EventoAdmin(admin.ModelAdmin):
     list_filter = ('tipo', 'data_inicio')
     search_fields = ('nome_evento',)
 
+
 @admin.register(Inscricao)
 class InscricaoAdmin(admin.ModelAdmin):
     list_display = ('id_usuario_fk', 'id_evento_fk', 'data_inscricao')
     ordering = ('-data_inscricao',)
 
+
 @admin.register(Certificado)
 class CertificadoAdmin(admin.ModelAdmin):
-    list_display = ('codigo_validacao', 'id_usuario_fk', 'id_evento_fk', 'data_emissao')
+    list_display = (
+        'codigo_validacao',
+        'id_usuario_fk',
+        'id_evento_fk',
+        'data_emissao'
+        )
     list_filter = ('data_emissao',)
