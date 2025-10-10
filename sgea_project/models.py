b943ea2e29efba94230a02795d61b7e8bfbcee41
gllugli <gabriel.lugli@gmail.com>
2025-10-10 11:39:05 -0300

    Organização do Arquivo seguindo a PEP8

--- sgea_project/models.py
+++ sgea_project/models.py
@@ -10,6 +10,7 @@ class Usuario(models.Model):
     senha = models.CharField(max_length=30)
     perfil = models.CharField(max_length=20, default='aluno')
 
+
 class Evento(models.Model):
     tipo = models.CharField(max_length=50)
     nome_evento = models.CharField(max_length=100, unique=True)
@@ -19,16 +20,23 @@ class Evento(models.Model):
     horario = models.TimeField()
     local_evento = models.CharField(max_length=50)
     qtd_participantes = models.IntegerField(null=True)
-    id_organizador_fk = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='eventos_organizados')
+    id_organizador_fk = models.ForeignKey(
+        Usuario, on_delete=models.CASCADE, related_name='eventos_organizados'
+        )
+
 
 class Inscricao(models.Model):
     data_inscricao = models.DateField(auto_now_add=True)
-    id_usuario_fk = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='inscricoes')
+    id_usuario_fk = models.ForeignKey(
+        Usuario, on_delete=models.CASCADE, related_name='inscricoes'
+        )
     id_evento_fk = models.ForeignKey(Evento, on_delete=models.CASCADE)
+
 
 class Certificado(models.Model):
     data_emissao = models.DateField()
     codigo_validacao = models.UUIDField(default=uuid.uuid4, unique=True)
-    id_usuario_fk = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='certificados')
+    id_usuario_fk = models.ForeignKey(
+        Usuario, on_delete=models.CASCADE, related_name='certificados'
+        )
     id_evento_fk = models.ForeignKey(Evento, on_delete=models.CASCADE)
-    
